7efeeccb4724e568f0cb56eb4cb7a7b33eea52c2
abhaymishra123 <abhay011235@gmail.com>
2024-10-23 19:46:38 +0530

    Free web Proxy 2 (hideme) added

--- FreeWebProxy.py
+++ FreeWebProxy.py
@@ -3,9 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-def proxy(driver, website):
+def proxy1(driver, website):
     driver.get("https://plainproxies.com/resources/free-web-proxy")
     time.sleep(1)
     driver.find_element(By.XPATH, "/html/body/section/div/div/div/form/div/input").send_keys(website)
     driver.find_element(By.XPATH, '''/html/body/section/div/div/div/form/div/button''').click()
+    time.sleep(5)
+
+def proxy2(driver,website):
+    driver.get("https://hide.me/en/proxy")
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="u"]').send_keys(website)
+    driver.find_element(By.XPATH, '//*[@id="hide_register_save"]').click()
     time.sleep(5)
