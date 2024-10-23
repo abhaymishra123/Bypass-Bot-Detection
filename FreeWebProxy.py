e3e0834315b35726914122bd5f9cf6b8f3933724
abhaymishra123 <abhay011235@gmail.com>
2024-10-23 19:40:09 +0530

    Free Web Proxy  1 (Plainproxies) added

--- FreeWebProxy.py
+++ FreeWebProxy.py
@@ -9,9 +9,3 @@ def proxy(driver, website):
     driver.find_element(By.XPATH, "/html/body/section/div/div/div/form/div/input").send_keys(website)
     driver.find_element(By.XPATH, '''/html/body/section/div/div/div/form/div/button''').click()
     time.sleep(5)
-def proxy(driver, website):
-    driver.get("https://plainproxies.com/resources/free-web-proxy")
-    time.sleep(1)
-    driver.find_element(By.XPATH, "/html/body/section/div/div/div/form/div/input").send_keys(website)
-    driver.find_element(By.XPATH, '''/html/body/section/div/div/div/form/div/button''').click()
-    time.sleep(5)
