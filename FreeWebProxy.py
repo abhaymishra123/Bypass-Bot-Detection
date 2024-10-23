fce6c6efa97eea901a9087a2d7e75bab05f59f7b
abhaymishra123 <abhay011235@gmail.com>
2024-10-23 19:36:17 +0530

    Free web Proxy 1 added

--- FreeWebProxy.py
+++ FreeWebProxy.py
@@ -1,4 +1,14 @@
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+def proxy(driver, website):
+    driver.get("https://plainproxies.com/resources/free-web-proxy")
+    time.sleep(1)
+    driver.find_element(By.XPATH, "/html/body/section/div/div/div/form/div/input").send_keys(website)
+    driver.find_element(By.XPATH, '''/html/body/section/div/div/div/form/div/button''').click()
+    time.sleep(5)
 def proxy(driver, website):
     driver.get("https://plainproxies.com/resources/free-web-proxy")
     time.sleep(1)
