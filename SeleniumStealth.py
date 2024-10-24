c2acd7df5f8e365941cb40ca6deb708d763543ff
abhaymishra123 <abhay011235@gmail.com>
2024-10-24 11:26:43 +0530

    SeleniumStealth approach

---
+++ SeleniumStealth.py
@@ -0,0 +1,24 @@
+from SeleniumStealth import stealth
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# Apply stealth configuration
+stealth(
+    driver,
+    languages=["en-US", "en"],
+    vendor="Google Inc.",
+    platform="Win32",
+    webgl_vendor="Intel Inc.",
+    renderer="Intel Iris OpenGL Engine",
+    fix_hairline=True,
+)
+
+# Example: Visit a website
+driver.get("https://example.com")
+print(driver.title)
